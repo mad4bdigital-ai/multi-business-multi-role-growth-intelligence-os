b39c976637b977d2d91c84aea2d90e424ec7d817
MAD4B <mad4b.digital@gmail.com>
2026-08-01 18:44:40 +0300

    fix(spec-014): bind repository brand to canonical persistence

--- http-generic-api/hostingerStorageControlPlaneRepository.js
+++ http-generic-api/hostingerStorageControlPlaneRepository.js
@@ -1,19 +1,38 @@
 import {
   HOSTINGER_STORAGE_CONTROL_PLANE_REPOSITORY_VERSION,
-  createMemoryHostingerStoragePersistenceAdapter,
+  createMemoryHostingerStoragePersistenceAdapter as createBaseMemoryHostingerStoragePersistenceAdapter,
   createHostingerStorageControlPlaneRepository as createBaseHostingerStorageControlPlaneRepository,
 } from './hostingerStorageControlPlaneRepositoryBase.js';
 
+const canonicalPersistenceAdapters = new WeakSet();
 const canonicalRepositories = new WeakSet();
 
-export {
-  HOSTINGER_STORAGE_CONTROL_PLANE_REPOSITORY_VERSION,
-  createMemoryHostingerStoragePersistenceAdapter,
-};
+export { HOSTINGER_STORAGE_CONTROL_PLANE_REPOSITORY_VERSION };
+
+export function createMemoryHostingerStoragePersistenceAdapter(options = {}) {
+  const adapter = createBaseMemoryHostingerStoragePersistenceAdapter(options);
+  canonicalPersistenceAdapters.add(adapter);
+  return adapter;
+}
+
+export function isCanonicalMemoryHostingerStoragePersistenceAdapter(adapter) {
+  return Boolean(
+    adapter
+    && canonicalPersistenceAdapters.has(adapter)
+    && Object.isFrozen(adapter)
+    && adapter.adapter_key === 'hostinger_storage_memory_test_adapter_v1'
+    && adapter.production_ready === false
+    && typeof adapter.transaction === 'function'
+    && typeof adapter.read === 'function'
+    && typeof adapter.export_snapshot === 'function'
+  );
+}
 
 export function createHostingerStorageControlPlaneRepository(options = {}) {
   const repository = createBaseHostingerStorageControlPlaneRepository(options);
-  canonicalRepositories.add(repository);
+  if (isCanonicalMemoryHostingerStoragePersistenceAdapter(options.adapter)) {
+    canonicalRepositories.add(repository);
+  }
   return repository;
 }
 
@@ -23,6 +42,7 @@ export function isCanonicalHostingerStorageControlPlaneRepository(repository) {
     && canonicalRepositories.has(repository)
     && Object.isFrozen(repository)
     && repository.repository_version === HOSTINGER_STORAGE_CONTROL_PLANE_REPOSITORY_VERSION
+    && repository.adapter_key === 'hostinger_storage_memory_test_adapter_v1'
     && repository.production_ready === false
     && typeof repository.readAggregate === 'function'
     && typeof repository.transitionOperation === 'function'
