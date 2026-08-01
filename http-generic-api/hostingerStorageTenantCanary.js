6cdff21e3969c9c13195bda473910055f33d9537
MAD4B <mad4b.digital@gmail.com>
2026-08-01 16:49:07 +0300

    fix(spec-014): adapt legacy authority updates to monotonic generations

--- http-generic-api/hostingerStorageTenantCanary.js
+++ http-generic-api/hostingerStorageTenantCanary.js
@@ -1,1 +1,30 @@
-export * from './hostingerStorageTenantCanaryV2.js';
+import {
+  createMemoryHostingerStorageTenantCanaryAuthorityStore as createV2AuthorityStore,
+} from './hostingerStorageTenantCanaryV2.js';
+
+export {
+  HOSTINGER_STORAGE_TENANT_CANARY_VERSION,
+  createMemoryHostingerStorageTenantCanaryEnablementRegistry,
+  executeHostingerStorageTenantCanary,
+} from './hostingerStorageTenantCanaryV2.js';
+
+export function createMemoryHostingerStorageTenantCanaryAuthorityStore() {
+  const store = createV2AuthorityStore();
+  return Object.freeze({
+    ...store,
+    updateAllowlist(input = {}) {
+      const current = store.readAllowlist(input.allowlist_id);
+      return store.updateAllowlist({
+        ...input,
+        expected_generation: input.expected_generation ?? current?.authority_generation,
+      });
+    },
+    updateApproval(input = {}) {
+      const current = store.readApproval(input.approval_id);
+      return store.updateApproval({
+        ...input,
+        expected_generation: input.expected_generation ?? current?.authority_generation,
+      });
+    },
+  });
+}
