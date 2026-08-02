6d6f449f7b2b32cd85ddcec84e92e96afccad44e
MAD4B <mad4b.digital@gmail.com>
2026-08-02 14:49:47 +0300

    feat(runtime): export managed execution recovery operations

--- http-generic-api/managedExecutionLifecycleService.js
+++ http-generic-api/managedExecutionLifecycleService.js
@@ -1,6 +1,7 @@
 export * from "./managedExecutionCore.js";
 export * from "./managedExecutionAuthority.js";
 export * from "./managedExecutionRunService.js";
+export * from "./managedExecutionRecoveryService.js";
 export { syncManagedExecutionRunStatus } from "./managedExecutionDecisionService.js";
 export {
   assertManagedExecutionApprovalAuthority,
